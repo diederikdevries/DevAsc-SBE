53e42feeda8fc771a5eb3f4adc68bf7d974c7b86
Diederik de Vries <git@diederik.nl>
2021-12-23 16:47:22 +0100

    Repair

--- Task 4 - Jenkins/Jenkins1.sh
+++ Task 4 - Jenkins/Jenkins1.sh
@@ -6,13 +6,13 @@ mkdir tempdir
 # cp sample_app.py tempdir/.
 # cp -r templates/* tempdir/templates/.
 # cp -r static/* tempdir/static/.
-FROM httpd:2-alpine >> tempdir/Dockerfile
-COPY ./index.html /usr/local/apache2/htdocs >> tempdir/Dockerfile
-RUN apk update >> tempdir/Dockerfile
-RUN sed -i 's@^#LoadModule rewrite_module modules/mod_rewrite\.so@LoadModule rewrite_module modules/mod_rewrite.so@' /usr/local/apache2/conf/httpd.conf >> tempdir/Dockerfile
-RUN sed -i 's/^Listen 80/Listen 8081/g' /usr/local/apache2/conf/httpd.conf >> tempdir/Dockerfile
-RUN sed -i 's/^<VirtualHost *:80>/<VirtualHost *:8081>/g' /usr/local/apache2/conf/extra/httpd-vhosts.conf >> tempdir/Dockerfile
-EXPOSE 8081/tcp >> tempdir/Dockerfile
+echo "FROM httpd:2-alpine" >> tempdir/Dockerfile
+echo "COPY ./index.html /usr/local/apache2/htdocs" >> tempdir/Dockerfile
+echo "RUN apk update" >> tempdir/Dockerfile
+echo "RUN sed -i 's@^#LoadModule rewrite_module modules/mod_rewrite\.so@LoadModule rewrite_module modules/mod_rewrite.so@' /usr/local/apache2/conf/httpd.conf" >> tempdir/Dockerfile
+echo "RUN sed -i 's/^Listen 80/Listen 8081/g' /usr/local/apache2/conf/httpd.conf" >> tempdir/Dockerfile
+echo "RUN sed -i 's/^<VirtualHost *:80>/<VirtualHost *:8081>/g' /usr/local/apache2/conf/extra/httpd-vhosts.conf" >> tempdir/Dockerfile
+echo "EXPOSE 8081/tcp" >> tempdir/Dockerfile
 # cd tempdir
 # docker build -t sampleapp .
 # docker run -t -d -p 5050:5050 --name samplerunning sampleapp
