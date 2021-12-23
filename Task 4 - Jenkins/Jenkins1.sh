74555a460fb2331246514c8f83727877131046d5
Diederik de Vries <git@diederik.nl>
2021-12-23 16:45:46 +0100

    Alleen dockerfile bouwen?

--- Task 4 - Jenkins/Jenkins1.sh
+++ Task 4 - Jenkins/Jenkins1.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 echo "Dit werkt"
-# mkdir tempdir
+mkdir tempdir
 # mkdir tempdir/templates
 # mkdir tempdir/static
 # cp sample_app.py tempdir/.
 # cp -r templates/* tempdir/templates/.
 # cp -r static/* tempdir/static/.
-# # echo "FROM python" >> tempdir/Dockerfile
-# # echo "RUN pip install flask" >> tempdir/Dockerfile
-# # echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
-# # echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-# # echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
-# # echo "EXPOSE 5050" >> tempdir/Dockerfile
-# # echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
+FROM httpd:2-alpine >> tempdir/Dockerfile
+COPY ./index.html /usr/local/apache2/htdocs >> tempdir/Dockerfile
+RUN apk update >> tempdir/Dockerfile
+RUN sed -i 's@^#LoadModule rewrite_module modules/mod_rewrite\.so@LoadModule rewrite_module modules/mod_rewrite.so@' /usr/local/apache2/conf/httpd.conf >> tempdir/Dockerfile
+RUN sed -i 's/^Listen 80/Listen 8081/g' /usr/local/apache2/conf/httpd.conf >> tempdir/Dockerfile
+RUN sed -i 's/^<VirtualHost *:80>/<VirtualHost *:8081>/g' /usr/local/apache2/conf/extra/httpd-vhosts.conf >> tempdir/Dockerfile
+EXPOSE 8081/tcp >> tempdir/Dockerfile
 # cd tempdir
 # docker build -t sampleapp .
 # docker run -t -d -p 5050:5050 --name samplerunning sampleapp
